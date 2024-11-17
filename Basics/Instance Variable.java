0d94cb1501a880c304f94e0b6a9f46c99665b7e1
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-11-17 22:44:33 +0530

    Create Instance Variable.java

---
+++ Basics/Instance Variable.java
@@ -0,0 +1,13 @@
+public class InstanceVariable {
+    
+    int b = 4;
+
+    public static void main(String[] args) {
+        
+        System.out.println(b);
+
+        /* Gives error because instance Variable can't be accessed with in methods directly. There is a way to call them
+           They can only be accessed by making objects
+        */
+    }
+}

b4f2c04255e329dfbb0edd0539ca1f05a018cc28
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-11-17 22:46:20 +0530

    Add files via upload

---
+++ Basics/StaticVariable.java
@@ -0,0 +1,14 @@
+public class StaticVariable {
+    
+    static int a = 5;
+
+    public static void main(String[] args) {
+
+        a = 10;
+        
+        System.out.println(a);
+
+        // By adding Static to instance or Global variable we can directly access them with in the methods also.
+
+    }
+}
